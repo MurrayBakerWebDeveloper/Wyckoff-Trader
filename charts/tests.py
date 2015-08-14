5c3c66857eb3aa90dba7206701e8651eda540ad5
Dadsaster <olliebradley76.com>
2015-08-13 19:32:06 -0700

    first unit_test passed!

--- charts/tests.py
+++ charts/tests.py
@@ -1,8 +1,9 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from charts.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
-
-
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
